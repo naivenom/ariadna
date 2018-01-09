b9746a3e62c59b896a535773aae89e4287cec41a
naivenom <naivenom@protonmail.com>
2018-01-09 21:30:38 +0100

    Add files via upload

---
+++ ariadna/ariadna/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ariadna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hTTPParametersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gETToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pOSTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hTTPParametersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1100, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hTTPParametersToolStripMenuItem
+            // 
+            this.hTTPParametersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gETToolStripMenuItem,
+            this.pOSTToolStripMenuItem});
+            this.hTTPParametersToolStripMenuItem.Name = "hTTPParametersToolStripMenuItem";
+            this.hTTPParametersToolStripMenuItem.Size = new System.Drawing.Size(135, 24);
+            this.hTTPParametersToolStripMenuItem.Text = "HTTP parameters";
+            this.hTTPParametersToolStripMenuItem.Click += new System.EventHandler(this.hTTPParametersToolStripMenuItem_Click);
+            // 
+            // gETToolStripMenuItem
+            // 
+            this.gETToolStripMenuItem.Name = "gETToolStripMenuItem";
+            this.gETToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.gETToolStripMenuItem.Text = "GET";
+            this.gETToolStripMenuItem.Click += new System.EventHandler(this.gETToolStripMenuItem_Click);
+            // 
+            // pOSTToolStripMenuItem
+            // 
+            this.pOSTToolStripMenuItem.Name = "pOSTToolStripMenuItem";
+            this.pOSTToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.pOSTToolStripMenuItem.Text = "POST";
+            this.pOSTToolStripMenuItem.Click += new System.EventHandler(this.pOSTToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(522, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(541, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(522, 190);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(519, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL/Path and Happy Fuzzing!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(519, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ariadna.Properties.Resources.ariadna;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(870, 418);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1100, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ariadna";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hTTPParametersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gETToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pOSTToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
